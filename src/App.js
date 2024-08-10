2987c2f1ff393966e12398dff9d3d80232c98db9
Mgridgelabs <mainaianndungu@gmail.com>
2024-08-10 20:35:59 +0300

    complete testing ui lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Maina Ian</h1>
+      <img src="" alt="your profile image" />
+      <h2>About Me</h2>
+      <p>My biography. I am a web dev</p>
+      <a href="https://github.com/yourusername">GitHub</a>
+      <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
+
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,56 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text `Hi,I'm ____`", () => {
+    render(<App />);
+
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+    expect(topLevelHeading).toBeInTheDocument();
+})
+
+test("displays an image with alt text identifying the content of the image", () => {
+    render(<App />);
+
+    const profileImage = screen.getByAltText(/your profile image/i);
+    expect(profileImage).toBeInTheDocument();
+    expect(profileImage).toHaveAttribute('src');
+});
+
+test("displays a second-level heading with the text `About Me`", () => {
+    render(<App />);
+
+    const aboutMeHeading = screen.getByRole("heading", { level: 2, name: /about me/i });
+
+    expect(aboutMeHeading).toBeInTheDocument();
+});
+
+
+test("displays a paragraph with biography text", () => {
+
+    render(<App />);
+
+    const biographyParagraph = screen.getByText(/my biography\. i am a web/i);
+
+    expect(biographyParagraph).toBeInTheDocument();
+});
+
+
+test("displays a link to GitHub page", () => {
+    render(<App />);
+    const githubLink = screen.getByRole("link", { name: /GitHub/i });
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink).toHaveAttribute('href', 'https://github.com/yourusername');
+});
+
+test("displays a link to LinkedIn page", () => {
+    render(<App />);
+    const linkedInlink = screen.getByRole("link", {
+        name: /linkedIn/i
+    });
+    expect(linkedInlink).toBeInTheDocument();
+    expect(linkedInlink).toHaveAttribute('href', 'https://linkedin.com/in/yourusername');
+});
